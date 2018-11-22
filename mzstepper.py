f23ce8b898b6eb1790b4ce1b434edd30c708ecea
Jairo Moreno <master.zion@gmail.com>
2018-11-22 03:12:19 +0100

    added thread support

--- mzstepper.py
+++ mzstepper.py
@@ -2,16 +2,16 @@
     File name: mzstepper.py
     Author: Jairo Moreno
     Date created: 20/11/2018
-    Date last modified: 20/11/2018
+    Date last modified: 22/11/2018
     Python Version: 3.6
+
     Based on: https://www.raspberrypi-spy.co.uk/2012/07/stepper-motor-control-in-python/
 '''
-
-
 
 # Import required libraries
 import sys
 import time
+import _thread
 import RPi.GPIO as GPIO
 
 
@@ -25,83 +25,88 @@ SeqDef =  [[1,0,0,1],
            [0,0,0,1]]
 
 class Stepper:
-
     def __init__(self, StepPins, CurrentPos = 0, MaxStep = 2000, WaitTime = 0.5, Debug = False, TurnOff = True, Seq = SeqDef ):
         self.MaxStep = MaxStep
         self.CurrentPos = CurrentPos
+        self.NewPos = CurrentPos
         self.WaitTime = WaitTime
         self.StepPins = StepPins
         self.Seq = Seq
         self.TurnOff = TurnOff
         self.Debug = Debug
 
+        GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         for pin in self.StepPins:
             GPIO.setup(pin,GPIO.OUT)
             GPIO.output(pin, False)
+        self.thread = _thread.start_new_thread(self.MoveMotor, () )
+
 
     def SetPosPercent(self, NewPosPercent):
         return self.SetPos( int(self.MaxStep  / 100 * float(NewPosPercent)) )
 
 
     def SetPos(self, NewPos):
-        if NewPos > self.MaxStep:
-            return self.CurrentPos
+        if self.NewPos > self.MaxStep:
+            return False
 
-        if NewPos < 0 :
-            return self.CurrentPos
+        if self.NewPos < 0 :
+            return False
 
-        if NewPos == self.CurrentPos :
-            return NewPos
+        self.NewPos = NewPos
 
-        if NewPos < self.CurrentPos :
-            StepDir = -1
-        else:
-            StepDir = 1
+    def GetPos(self):
+        return self.CurrentPos
 
-        StepCount = len(self.Seq)
+
+    def MoveMotor(self):
 
         # Initialise variables
         StepCounter = 0
+        StepCount = len(self.Seq)
+        pin_len=len(self.Seq[0])
 
         # Start main loop
-        if self.Debug:
-           print("while not " + str(NewPos) + " == " + str(self.CurrentPos))
-
-        while not NewPos == self.CurrentPos:
-            if self.Debug:
-                print(StepCounter)
-                print(self.Seq[StepCounter])
-
-            for pin in range(0, 4):
-                xpin = self.StepPins[pin]
-                if self.Seq[StepCounter][pin]!=0:
-                    if self.Debug:
-                        print(str(self.CurrentPos) + " Enable GPIO %i"  %(xpin))
-                    GPIO.output(xpin, True)
-                else:
-                    GPIO.output(xpin, False)
-
-            StepCounter += StepDir
-            self.CurrentPos += StepDir
-
-            # If we reach the end of the sequence
-            # start again
-            if (StepCounter>=StepCount):
-                StepCounter = 0
-            if (StepCounter<0):
-                StepCounter = StepCount+StepDir
-
+        while True:
             # Wait before moving on
             time.sleep(self.WaitTime)
 
-            for pin in self.StepPins:
-                GPIO.output(pin, False)
-        return self.CurrentPos
+            if self.NewPos == self.CurrentPos:
+                if self.TurnOff:
+                    self.Disable()
+            else:
+                if self.NewPos < self.CurrentPos :
+                    StepDir = -1
+                else:
+                    StepDir = 1
+
+                if self.Debug:
+                    print("StepCounter: "+str(StepCounter) )
+                    print(self.Seq[StepCounter])
+
+                for pin in range(0, pin_len):
+                    xpin = self.StepPins[pin]
+                    if self.Seq[StepCounter][pin]!=0:
+                        if self.Debug:
+                            print(str(self.CurrentPos) + " Enable GPIO %i"  %(xpin))
+                        GPIO.output(xpin, True)
+                    else:
+                        GPIO.output(xpin, False)
+
+                StepCounter += StepDir
+                self.CurrentPos += StepDir
+
+                # If we reach the end of the sequence
+                # start again
+                if (StepCounter>=StepCount):
+                    StepCounter = 0
+                if (StepCounter<0):
+                    StepCounter = StepCount+StepDir
+        return True
 
     def Disable(self):
-        if self.TurnOff:
-            for pin in self.StepPins:
-                if self.Debug:
-                    print("disable pins")
-                GPIO.output(pin, False)
+        for pin in self.StepPins:
+            if self.Debug:
+                print("disable pins")
+            GPIO.output(pin, False)
--- test.py
+++ test.py
@@ -2,14 +2,27 @@
     File name: test.py
     Author: Jairo Moreno
     Date created: 20/11/2018
-    Date last modified: 20/11/2018
+    Date last modified: 22/11/2018
     Python Version: 3.6
 '''
 
 import mzstepper
+import time
+import random
 
 StepPins =  [26,19,13,6]
-obj = mzstepper.Stepper(StepPins, 0, 2000, 0.001, True)
-print(obj.SetPosPercent(90) )
-print(obj.SetPosPercent(20) )
+obj = mzstepper.Stepper(StepPins, 0, 2000, 0.001, True, True)
+
+print("pos: 100")
+obj.SetPosPercent(100)
+for x in range(10):
+    pos = random.randint(1, 100)
+    print("pos: "+str(pos)+" - "+str(obj.GetPos()) )
+    obj.SetPosPercent(pos)
+    time.sleep(1)
+
+print("pos : 0")
+obj.SetPosPercent(0)
+time.sleep(3)
 obj.Disable()
+time.sleep(1)
